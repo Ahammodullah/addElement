67aecbb1a31a2d1ab5896f302b5dea6da9c836bf
Ahammod <ahammodullah12@gmail.com>
2024-09-19 22:52:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function addStyle(){
+
+    var myVar= document.querySelector("#paraid");
+    myVar.classList.add("para_style");
+}
+
+
+function removeStyle(){
+
+    var myVar= document.querySelector("#paraid");
+    myVar.classList.remove("para_style");
+}
